2b913a531b9875b08c0dba035d159a8ba77fdadc
IKalman <iankalman04@gmail.com>
2025-11-04 13:33:42 -0500

    adde led4

--- ledBlink.ino
+++ ledBlink.ino
@@ -1,20 +1,24 @@
 int ledPin = 13;
 int ledPin2 = 14;
 int ledPin3 = 15;
+int ledPin4 = 15;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
 }
 
 void loop() {
   digitalWrite(ledPin, HIGH);
   digitalWrite(ledPin2, HIGH);
   digitalWrite(ledPin3, HIGH);
+  digitalWrite(ledPin4, HIGH);
   delay(500);
   digitalWrite(ledPin, LOW);
   digitalWrite(ledPin2, LOW);
   digitalWrite(ledPin3, LOW);
+  digitalWrite(ledPin4, LOW);
   delay(500);
 }
